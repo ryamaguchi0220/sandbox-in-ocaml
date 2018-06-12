ad08a2a66a7ff951e8aec083c8fe26bb4cc5d499
ryamaguchi0220 <maguchi579@gmail.com>
2018-06-12 23:48:34 +0900

    不要なファイルを削除

--- src/seiza.ml
+++
@@ -1,9 +0,0 @@
-(* 目的：時間xに応じて午前か午後を返す *)
-(* jikan：int -> string *)
-let jikan x  = if x < 12 then "午前" else "午後"
-
-(* テスト *)
-let test1 = jikan 11 = "午前"
-let test2 = jikan 12 = "午後"
-let test3 = jikan 5 = "午前"
-let test4 = jikan 15 = "午後"
