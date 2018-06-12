9529d054490e6e335c5a936df5629646c90f239d
ryamaguchi0220 <maguchi579@gmail.com>
2018-06-12 23:54:26 +0900

    doc修正

--- src/fac.ml
+++ src/fac.ml
@@ -1,8 +1,8 @@
 let rec enumrate n = if n = 0 then []
 	else n :: enumrate (n - 1)
 
-(* 1から受け取った自然数nまでの合計を返す *)
-(* one_to_n: int -> int *)
+(* nの累乗を返す *)
+(* fac: int -> int *)
 let fac n = List.fold_right ( * ) (enumrate n) 1 
 
 let test2 = fac 1 = 1
